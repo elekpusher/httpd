e9a1c2bcec55f43238b71b26365461409a20d5cc
elek <robbiemay501@gmail.com>
2017-01-27 13:30:20 +0100

    Update c9.sh

--- c9.sh
+++ c9.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-if [ $1 == "start" ]; then
+if [ "$1" == "start" ]; then
 docker run --name cloud9 -d -p 81:80 -v `pwd`:/workspace/ kdelfour/cloud9-docker
 exit 0
 fi
-if [ $1 == "stop" ]; then
+if [ "$1" == "stop" ]; then
 docker rm -f cloud9
 exit 0
 fi
